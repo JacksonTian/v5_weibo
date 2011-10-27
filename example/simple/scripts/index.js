776f28eef84eb81399aede911cd3a6de497ed062
Jackson Tian <shyvo1987@gmail.com>
2011-10-27 23:58:17 +0800

    Update.

--- example/simple/scripts/index.js
+++
@@ -1,8 +0,0 @@
-V5.registerPage("index", function () {
-    var initialize = function () {
-        // Write down your initialize code at here.
-    };
-    return {
-        initialize: initialize
-    };
-});
